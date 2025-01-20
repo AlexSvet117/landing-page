54d19e1aa3a6902205bb01e81087d1e0e1a44477
AlexSvet117 <alex.svet117@gmail.com>
2025-01-20 19:20:31 +0000

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import Logo from './components/Logo';
+import Card from './components/Card.jsx';
+import Footer from './components/Footer.jsx';
+import Hero from './components/Hero.jsx';
+import Nav from './components/Nav.jsx';
+import CardContainer from './components/CardContainer.jsx';
 
 function App() {
+
+  const descriptions = [
+    "This is the description of the card",
+    "This is the description of the card 2",
+    "This is the description of the card 3",
+    "This is the description of the card 4"
+  ]
+
+
   return (
     <>
-      <Logo />
+      <Nav/>
+      <Hero/>
+      <CardContainer>
+        <Card titleName="Nice View" text={descriptions[0]} src="https://picsum.photos/200?random=1"/>
+        <Card titleName="Awesome View" text={descriptions[1]} src="https://picsum.photos/200?random=2"/>
+        <Card titleName="Excellent View" text={descriptions[2]} src="https://picsum.photos/200?random=3"/>
+        <Card titleName="Perfect View" text={descriptions[2]} src="https://picsum.photos/200?random=4"/>
+        <Card titleName="Awesome View" text={descriptions[1]} src="https://picsum.photos/200?random=5"/>
+        <Card titleName="Excellent View" text={descriptions[2]} src="https://picsum.photos/200?random=6"/>
+        <Card titleName="Perfect View" text={descriptions[2]} src="https://picsum.photos/200?random=7"/>
+      </CardContainer>
+      <Footer/>
     </>
   );
 }
